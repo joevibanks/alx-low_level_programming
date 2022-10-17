575493acaf39f5682ef261d1b1f01a201549d97e
root <joevibanks@gmail.com>
2022-10-17 06:41:04 -0700

    first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * Main - prints a line line of code the put function
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle.");
+            retun (0);
+}
