31a45a52050d5283652625ec7d1e15bcab1581e5
root <joevibanks@gmail.com>
2022-10-18 15:34:45 -0700

    fil your ghost

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is positve\n", n);
+printf("%d is positive\n", n);
 else if (n < 0)
 printf("%d is negative\n", n);
 else
