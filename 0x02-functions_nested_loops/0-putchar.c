aff7d05f87baa6aaefaec7bbfb2cd8e2d66ab272
root <joevibanks@gmail.com>
2022-10-19 16:11:11 -0700

    second attempt

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,12 @@
 #include "main.h"
+#include <unistd.h>
 /**
- * main -print _putchar
+ * _putchar -prints out all the character c to standout
  * code written by joevi
- * Return: 0
+ * Return: On success 1
  */
 
-int main(void)
+int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+return (write(1, &c, 1));
 }
